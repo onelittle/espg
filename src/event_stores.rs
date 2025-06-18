dfab228385fdaf57cefbb4044503abd8bfa7718f
Theodor Tonum <theodor@tonum.no>
2025-06-19 00:24:24 +0200

    move streaming to module

--- src/event_stores.rs
+++ src/event_stores.rs
@@ -4,6 +4,8 @@ mod in_memory;
 pub mod postgres;
 #[cfg(feature = "postgres")]
 pub(crate) mod sql_helpers;
+#[cfg(feature = "streaming")]
+mod streaming;
 
 use crate::Aggregate;
 #[cfg(feature = "inmem")]
@@ -11,6 +13,8 @@ pub use in_memory::InMemoryEventStore;
 #[cfg(feature = "postgres")]
 pub use postgres::PostgresEventStore;
 use serde::{Deserialize, Serialize};
+#[cfg(feature = "streaming")]
+pub use streaming::EventStream;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -123,78 +127,3 @@ pub trait StreamingEventStore<'a, T: Aggregate + Default> {
         id: &str,
     ) -> Result<EventStream<Self::StreamReceiver, Self::StreamClient>>;
 }
-
-#[cfg(feature = "streaming")]
-mod streaming {
-    use std::{
-        pin::Pin,
-        sync::mpsc::Receiver,
-        task::{Context, Poll},
-    };
-    use tokio::sync::mpsc::UnboundedReceiver;
-    use tokio_stream::Stream;
-
-    #[derive(Debug)]
-    pub struct EventStream<R, X> {
-        inner: R,
-        _client: X,
-    }
-
-    impl<R, X> EventStream<R, X> {
-        /// Create a new `UnboundedReceiverStream`.
-        pub fn new(recv: R, client: X) -> Self {
-            Self {
-                inner: recv,
-                _client: client,
-            }
-        }
-
-        /// Get back the inner `UnboundedReceiver`.
-        pub fn into_inner(self) -> R {
-            self.inner
-        }
-    }
-
-    impl<T, X> EventStream<Receiver<T>, X> {
-        /// Closes the receiving half of a channel without dropping it.
-        ///
-        /// This prevents any further messages from being sent on the channel while
-        /// still enabling the receiver to drain messages that are buffered.
-        pub fn close(&mut self) {
-            // Note: This is a no-op for `std::sync::mpsc::Receiver` since it does not have a close method.
-        }
-    }
-
-    impl<T, X> EventStream<UnboundedReceiver<T>, X> {
-        /// Closes the receiving half of a channel without dropping it.
-        ///
-        /// This prevents any further messages from being sent on the channel while
-        /// still enabling the receiver to drain messages that are buffered.
-        pub fn close(&mut self) {
-            self.inner.close();
-        }
-    }
-
-    impl<T, X: Unpin> Stream for EventStream<UnboundedReceiver<T>, X> {
-        type Item = T;
-
-        fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-            self.inner.poll_recv(cx)
-        }
-    }
-
-    impl<T, X> AsRef<UnboundedReceiver<T>> for EventStream<UnboundedReceiver<T>, X> {
-        fn as_ref(&self) -> &UnboundedReceiver<T> {
-            &self.inner
-        }
-    }
-
-    impl<T, X> AsMut<UnboundedReceiver<T>> for EventStream<UnboundedReceiver<T>, X> {
-        fn as_mut(&mut self) -> &mut UnboundedReceiver<T> {
-            &mut self.inner
-        }
-    }
-}
-
-#[cfg(feature = "streaming")]
-pub use streaming::EventStream;
---
+++ src/event_stores/streaming.rs
@@ -0,0 +1,68 @@
+use std::{
+    pin::Pin,
+    sync::mpsc::Receiver,
+    task::{Context, Poll},
+};
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio_stream::Stream;
+
+#[derive(Debug)]
+pub struct EventStream<R, X> {
+    inner: R,
+    _client: X,
+}
+
+impl<R, X> EventStream<R, X> {
+    /// Create a new `UnboundedReceiverStream`.
+    pub fn new(recv: R, client: X) -> Self {
+        Self {
+            inner: recv,
+            _client: client,
+        }
+    }
+
+    /// Get back the inner `UnboundedReceiver`.
+    pub fn into_inner(self) -> R {
+        self.inner
+    }
+}
+
+impl<T, X> EventStream<Receiver<T>, X> {
+    /// Closes the receiving half of a channel without dropping it.
+    ///
+    /// This prevents any further messages from being sent on the channel while
+    /// still enabling the receiver to drain messages that are buffered.
+    pub fn close(&mut self) {
+        // Note: This is a no-op for `std::sync::mpsc::Receiver` since it does not have a close method.
+    }
+}
+
+impl<T, X> EventStream<UnboundedReceiver<T>, X> {
+    /// Closes the receiving half of a channel without dropping it.
+    ///
+    /// This prevents any further messages from being sent on the channel while
+    /// still enabling the receiver to drain messages that are buffered.
+    pub fn close(&mut self) {
+        self.inner.close();
+    }
+}
+
+impl<T, X: Unpin> Stream for EventStream<UnboundedReceiver<T>, X> {
+    type Item = T;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.inner.poll_recv(cx)
+    }
+}
+
+impl<T, X> AsRef<UnboundedReceiver<T>> for EventStream<UnboundedReceiver<T>, X> {
+    fn as_ref(&self) -> &UnboundedReceiver<T> {
+        &self.inner
+    }
+}
+
+impl<T, X> AsMut<UnboundedReceiver<T>> for EventStream<UnboundedReceiver<T>, X> {
+    fn as_mut(&mut self) -> &mut UnboundedReceiver<T> {
+        &mut self.inner
+    }
+}
