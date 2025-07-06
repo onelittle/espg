cfbf41da01cbd77a8cded40e3dbf264f99d5209e
Theodor Tonum <theodor@tonum.no>
2025-07-06 23:39:29 +0200

    commit ergonomics

--- src/commit.rs
+++ src/commit.rs
@@ -12,14 +12,37 @@ pub(crate) struct Diagnostics {
 pub struct Commit<T> {
     pub id: String,
     pub version: usize,
+    #[serde(flatten)]
     pub inner: T,
+    #[serde(skip)]
     pub(crate) diagnostics: Option<Diagnostics>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) global_seq: Option<Txid>,
 }
 
 impl<T> Commit<T> {
+    pub fn new(id: impl Into<String>, version: usize, inner: T) -> Self {
+        Self {
+            id: id.into(),
+            version,
+            inner,
+            diagnostics: None,
+            global_seq: None,
+        }
+    }
+
     pub fn into_inner(self) -> T {
         self.inner
+    }
+
+    pub fn with_inner<U>(self, inner: U) -> Commit<U> {
+        Commit {
+            id: self.id,
+            version: self.version,
+            inner,
+            diagnostics: self.diagnostics,
+            global_seq: self.global_seq,
+        }
     }
 }
 
