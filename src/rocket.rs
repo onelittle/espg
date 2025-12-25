38f6e6359f41f23ee073b23bb9fa99277a69f4bc
Theodor Tonum <theodor@tonum.no>
2025-12-25 19:42:32 +0100

    mark Error infallible

--- src/rocket.rs
+++ src/rocket.rs
@@ -1,10 +1,10 @@
-use std::marker::PhantomData;
+use std::{convert::Infallible, marker::PhantomData};
 
 use crate::Id;
 use rocket::request::FromParam;
 
 impl<T> FromParam<'_> for Id<T> {
-    type Error = String;
+    type Error = Infallible;
 
     fn from_param(param: &str) -> Result<Self, Self::Error> {
         Ok(Id(param.to_string(), PhantomData))
